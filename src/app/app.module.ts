39afb0fc5a965fff8fb8efe476b650ac1bd8e885
Clement rouvier <clem.rouv30@gmail.com>
2021-12-07 14:11:48 +0100

    v1.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AcceuilComponent } from './acceuil/acceuil.component';
+import { StatistiquesComponent } from './statistiques/statistiques.component';
+import { ShopComponent } from './shop/shop.component';
+import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcceuilComponent,
+    StatistiquesComponent,
+    ShopComponent,
+    AboutComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
